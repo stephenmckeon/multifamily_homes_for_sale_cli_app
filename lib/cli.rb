c9e1302e25feef70b70fd956f6272c61b2af736b
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-19 10:03:13 -0400

    Refactor #start

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,7 +7,6 @@ class Cli
   # --IDEAS--
   ## have a profile setup with password that knows your default seach area
   ## have guest profile with no password that makes you select an area to search
-  # use user input (address) to display more info  about the home
 
   def call
     Scraper.new.scrape_listings
@@ -23,9 +22,13 @@ class Cli
   def start
     display_properties
     prompt_user
+    input
     until user_input_exit?
+      invalid_address?
       display_details
+      start if user_input_back?
     end
+    exit
   end
 
   def display_properties
@@ -42,33 +45,19 @@ class Cli
 
   def prompt_user
     puts "To see more information on a property, type its address and press enter."
-    puts "To exit"
+    puts "To exit, type 'exit'."
     puts
-    input
   end
 
   def input
     @user_input = gets.strip
-    puts
-    exit if user_input_exit?
-
-    invalid_address if Scraper.find_property(@user_input).nil?
   end
 
   def display_details
     puts
     Scraper.scrape_property_details(@user_input)
     puts
-    puts "Typ 'back' to go back to the listings page. Tpye 'exit' to exit."
-    puts
-    @user_input = gets.strip
-
-    start if @user_input == "back"
-  end
-
-  def invalid_address
-    puts "Invalid address..."
-    puts "Please type the address EXACTLY as seen in the listing."
+    puts "Type 'back' to go back to the listings page or type 'exit' to exit."
     puts
     input
   end
@@ -78,8 +67,23 @@ class Cli
     puts
   end
 
+  def invalid_address?
+    if Scraper.find_property(@user_input).nil?
+      puts
+      puts "Invalid input..."
+      puts "Please type the address EXACTLY as seen in the listing"
+      puts "or type a valid command. Thank-you!"
+      puts
+      input
+    end
+  end
+
   def user_input_exit?
     @user_input.casecmp("exit").zero?
+  end
+
+  def user_input_back?
+    @user_input.casecmp("back").zero?
   end
 
   def exit
